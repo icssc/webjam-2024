dc13f6c1c1218d34894a3d52a0c9617d640b6614
Kevin Wu <kevinwu098@gmail.com>
2024-11-05 18:29:47 -0800

    initial (big) commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,94 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { CalendarIcon, MapPinnedIcon } from "lucide-react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    return (
+        <div className="h-[100dvh] bg-red-400 text-neutral-100">
+            <div className="grid h-[100dvh] grid-cols-11 grid-rows-11">
+                <div className="z-10 col-start-6 row-start-5 flex flex-col items-center justify-center space-y-8">
+                    <div className="flex flex-col items-center space-y-2">
+                        <h1 className="col-span-7 col-start-4 w-fit whitespace-nowrap text-9xl font-bold">
+                            WebJam 2024
+                        </h1>
+                        <p className="w-[80%] text-center text-2xl font-semibold">
+                            ICS Student Council&apos;s week-long web application
+                            competition — no experience necessary!
+                        </p>
+                    </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+                    <div className="flex w-[400%] flex-row justify-center space-x-8">
+                        <div className="flex min-w-fit items-center space-x-2 text-2xl font-semibold">
+                            <CalendarIcon />
+
+                            <p>Nov. 18 - 22</p>
+                        </div>
+
+                        <div className="flex min-w-fit items-center space-x-2 text-2xl font-semibold">
+                            <MapPinnedIcon />
+
+                            <p>Donald Bren Hall</p>
+                        </div>
+                    </div>
+
+                    <div className="flex items-center space-x-4 text-2xl font-semibold">
+                        <Button className="border-2 bg-red-400 px-3 py-4 text-2xl font-semibold text-white hover:bg-red-300">
+                            Apply
+                        </Button>
+
+                        <p>or</p>
+
+                        <Button className="border-2 bg-red-400 px-3 py-4 text-2xl font-semibold text-white hover:bg-red-300">
+                            Mentor
+                        </Button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="absolute top-0 h-[100dvh] w-[100dvw]">
+                <Image
+                    src={"/layer-one/L1_MountainBackground.svg"}
+                    alt={""}
+                    width={2000}
+                    height={100}
+                    className="absolute bottom-0 w-[55%]"
+                />
+                <Image
+                    src={"/layer-one/L1_MountainForeground.svg"}
+                    alt={"mountain foreground"}
+                    width={2000}
+                    height={100}
+                    className="absolute bottom-0 w-full"
+                />
+                <Image
+                    src={"/layer-one/L1_Sun.svg"}
+                    alt={""}
+                    width={400}
+                    height={100}
+                    className="absolute right-[8%] top-[8%] w-[12.5%]"
+                />
+                <Image
+                    src={"/layer-one/L1_SweatyAnteater.svg"}
+                    alt={""}
+                    width={400}
+                    height={100}
+                    className="absolute bottom-[14%] left-[16%] w-[15%]"
+                />
+                <Image
+                    src={"/layer-one/L1_HelpfulAnteater.svg"}
+                    alt={""}
+                    width={400}
+                    height={100}
+                    className="absolute bottom-[18%] left-[32%] w-[12.5%]"
+                />
+                <Image
+                    src={"/layer-one/L1_BuffAnteater.svg"}
+                    alt={""}
+                    width={400}
+                    height={100}
+                    className="absolute bottom-[36%] right-[12%] w-[12.5%]"
+                />
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
