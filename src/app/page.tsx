5aa200ddf4daac565fae242453006711269cf2f9
Kevin Wu <kevinwu098@gmail.com>
2024-11-06 17:49:00 -0800

    feat: dev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import { CalendarIcon, MapPinnedIcon } from "lucide-react";
 
 export default function Home() {
     return (
-        <div className="h-[100dvh] bg-red-400 text-neutral-100">
+        <div className="relative flex h-[100dvh] max-h-[1000px] items-center justify-center bg-red-400 text-neutral-100">
             <div className="grid h-[100dvh] grid-cols-11 grid-rows-11">
                 <div className="z-10 col-start-6 row-start-5 flex flex-col items-center justify-center space-y-8">
                     <div className="flex flex-col items-center space-y-2">
-                        <h1 className="col-span-7 col-start-4 w-fit whitespace-nowrap text-9xl font-bold">
+                        <h1 className="col-span-7 col-start-4 w-fit whitespace-nowrap text-[11rem] font-bold leading-none tracking-tight">
                             WebJam 2024
                         </h1>
-                        <p className="w-[80%] text-center text-2xl font-semibold">
+                        <p className="w-[50%] text-center text-2xl font-semibold">
                             ICS Student Council&apos;s week-long web application
                             competition — no experience necessary!
                         </p>
@@ -45,48 +46,62 @@ export default function Home() {
                 </div>
             </div>
 
-            <div className="absolute top-0 h-[100dvh] w-[100dvw]">
+            <div className="absolute top-0 h-[100dvh] max-h-[1000px] w-full">
                 <Image
                     src={"/layer-one/L1_MountainBackground.svg"}
                     alt={""}
                     width={2000}
                     height={100}
-                    className="absolute bottom-0 w-[55%]"
+                    className="absolute bottom-0 right-0 w-[1000px] min-w-[700px] md:right-auto"
                 />
+
                 <Image
                     src={"/layer-one/L1_MountainForeground.svg"}
                     alt={"mountain foreground"}
                     width={2000}
                     height={100}
-                    className="absolute bottom-0 w-full"
+                    className="absolute bottom-0 right-0 w-[1728px] min-w-[1000px]"
                 />
-                <Image
-                    src={"/layer-one/L1_Sun.svg"}
-                    alt={""}
-                    width={400}
-                    height={100}
-                    className="absolute right-[8%] top-[8%] w-[12.5%]"
-                />
+
                 <Image
                     src={"/layer-one/L1_SweatyAnteater.svg"}
                     alt={""}
                     width={400}
                     height={100}
-                    className="absolute bottom-[14%] left-[16%] w-[15%]"
+                    className={cn(
+                        "absolute bottom-[10%] left-[16%] w-[15%]",
+                        "hidden md:flex"
+                    )}
                 />
                 <Image
                     src={"/layer-one/L1_HelpfulAnteater.svg"}
                     alt={""}
                     width={400}
                     height={100}
-                    className="absolute bottom-[18%] left-[32%] w-[12.5%]"
+                    className={cn(
+                        "absolute bottom-[18%] left-[32%] w-[12.5%]",
+                        "hidden md:flex"
+                    )}
                 />
+
                 <Image
                     src={"/layer-one/L1_BuffAnteater.svg"}
                     alt={""}
                     width={400}
                     height={100}
                     className="absolute bottom-[36%] right-[12%] w-[12.5%]"
+                />
+
+                <Image
+                    src={"/layer-one/L1_Sun.svg"}
+                    alt={""}
+                    width={400}
+                    height={100}
+                    className={cn(
+                        "absolute",
+                        "right-[4%] top-[4%] md:right-[8%] md:top-[8%]",
+                        "w-[25%] md:w-[12.5%]"
+                    )}
                 />
             </div>
         </div>
