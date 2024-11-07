35620fdc575ef639856c0535418135b635944d6c
Kevin Wu <kevinwu098@gmail.com>
2024-11-06 18:30:02 -0800

    feat: text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,43 +5,75 @@ import { CalendarIcon, MapPinnedIcon } from "lucide-react";
 
 export default function Home() {
     return (
-        <div className="relative flex h-[100dvh] max-h-[1000px] items-center justify-center bg-red-400 text-neutral-100">
+        <div className="relative flex h-[100dvh] max-h-[1000px] items-center justify-center overflow-hidden bg-red-400 text-neutral-100">
             <div className="grid h-[100dvh] grid-cols-11 grid-rows-11">
-                <div className="z-10 col-start-6 row-start-5 flex flex-col items-center justify-center space-y-8">
+                <div
+                    className={cn(
+                        "z-10 col-start-6 flex w-full flex-col items-center justify-center space-y-8",
+                        "row-start-5"
+                    )}
+                >
                     <div className="flex flex-col items-center space-y-2">
-                        <h1 className="col-span-7 col-start-4 w-fit whitespace-nowrap text-[11rem] font-bold leading-none tracking-tight">
+                        <h1
+                            className={cn(
+                                "col-span-7 col-start-4 w-fit whitespace-nowrap font-bold tracking-tight",
+                                "text-[clamp(2rem,14vw,11rem)] leading-none"
+                            )}
+                        >
                             WebJam 2024
                         </h1>
-                        <p className="w-[50%] text-center text-2xl font-semibold">
+                        <p
+                            className={cn(
+                                "text-center font-semibold",
+                                "w-[90%] md:w-[75%] lg:w-[75%]",
+                                "text-[clamp(1.25rem,3vw,1.5rem)]"
+                            )}
+                        >
                             ICS Student Council&apos;s week-long web application
                             competition — no experience necessary!
                         </p>
                     </div>
 
-                    <div className="flex w-[400%] flex-row justify-center space-x-8">
-                        <div className="flex min-w-fit items-center space-x-2 text-2xl font-semibold">
+                    <div className="flex flex-col items-center justify-center space-x-8 space-y-2 md:flex-row">
+                        <div
+                            className={cn(
+                                "flex min-w-fit items-center space-x-2 font-semibold",
+                                "text-[clamp(1.25rem,3vw,1.5rem)]"
+                            )}
+                        >
                             <CalendarIcon />
 
-                            <p>Nov. 18 - 22</p>
+                            <p className="whitespace-nowrap">Nov. 18 - 22</p>
                         </div>
 
-                        <div className="flex min-w-fit items-center space-x-2 text-2xl font-semibold">
+                        <div
+                            className={cn(
+                                "flex min-w-fit items-center space-x-2 font-semibold",
+                                "text-[clamp(1.25rem,3vw,1.5rem)]"
+                            )}
+                        >
                             <MapPinnedIcon />
 
-                            <p>Donald Bren Hall</p>
+                            <p className="whitespace-nowrap">
+                                Donald Bren Hall
+                            </p>
                         </div>
                     </div>
 
-                    <div className="flex items-center space-x-4 text-2xl font-semibold">
-                        <Button className="border-2 bg-red-400 px-3 py-4 text-2xl font-semibold text-white hover:bg-red-300">
+                    <div className="flex items-center space-x-2 text-2xl font-semibold">
+                        <Button className="border-2 bg-red-400 bg-opacity-60 px-3 py-4 text-2xl font-semibold text-white backdrop-blur-lg hover:bg-red-300 hover:bg-opacity-70">
                             Apply
                         </Button>
 
-                        <p>or</p>
-
-                        <Button className="border-2 bg-red-400 px-3 py-4 text-2xl font-semibold text-white hover:bg-red-300">
-                            Mentor
-                        </Button>
+                        <div className="flex items-center">
+                            <p className="whitespace-nowrap">or be a &nbsp;</p>
+                            <Button
+                                className="p-0 text-2xl font-semibold text-white underline hover:decoration-red-300"
+                                variant={"link"}
+                            >
+                                Mentor!
+                            </Button>
+                        </div>
                     </div>
                 </div>
             </div>
